0230117a255e00b37d083c037b444cf7f926e66b
Aman Sharma <mannu.poski10@gmail.com>
2018-08-03 17:29:15 +0530

    Polls view modified

--- frontend/views/polls/index.php
+++ frontend/views/polls/index.php
@@ -1,1 +1,15 @@
-<?= 'hello'; ?>
+<?php 
+    foreach($questions as $question)
+    {
+        echo '<h5>'.$question->question_text.'</h5>';
+        echo '<ol>';
+        foreach($choices as $choice)
+        {
+            if($choice->question_id === $question->id)
+            {
+                echo '<li>'.$choice->choice_text.'</li>';
+            }
+        }
+        echo '</ol>';
+    }
+?>
