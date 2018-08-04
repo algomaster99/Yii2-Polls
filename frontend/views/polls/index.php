f51f40780a41683bd425df65b9f339b46c6312b6
Aman Sharma <mannu.poski10@gmail.com>
2018-08-04 14:29:00 +0530

    Created a link on index to view detailView

--- frontend/views/polls/index.php
+++ frontend/views/polls/index.php
@@ -1,15 +1,19 @@
+<?php
+use yii\helpers\Html;
+?>
+
 <?php 
     foreach($questions as $question)
     {
-        echo '<h5>'.$question->question_text.'</h5>';
-        echo '<ol>';
-        foreach($choices as $choice)
-        {
-            if($choice->question_id === $question->id)
-            {
-                echo '<li>'.$choice->choice_text.'</li>';
-            }
-        }
-        echo '</ol>';
+        echo Html::a('<h3>'.$question->question_text.'</h3>', ['/polls/question-detail-view', 'id'=>$question->id]);
+        // echo '<ol>';
+        // foreach($choices as $choice)
+        // {
+        //     if($choice->question_id === $question->id)
+        //     {
+        //         echo '<li>'.$choice->choice_text.'</li>';
+        //     }
+        // }
+        // echo '</ol>';
     }
 ?>
