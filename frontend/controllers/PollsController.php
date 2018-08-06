854d340a4d0e18ea6cb5e29a79b86fe839909e55
Aman Sharma <mannu.poski10@gmail.com>
2018-08-07 01:13:51 +0530

    Added AccessController for polls app

--- frontend/controllers/PollsController.php
+++ frontend/controllers/PollsController.php
@@ -4,11 +4,47 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use common\components\AccessRule;
+use common\models\User;
 use common\models\QuestionForm;
 use common\models\ChoiceForm;
 
 class PollsController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'ruleConfig' => [
+                    'class' => AccessRule::className(),
+                ],
+                'only' => ['index', 'ask', 'choice', 'question-detail-view'],
+                'rules' => [
+                    [
+                        'actions' => ['index', 'question-detail-view'],
+                        'allow' => true,
+                        'roles' => [
+                            User::ROLE_USER,
+                            User::ROLE_ADMIN,
+                        ],
+                    ],
+                    [
+                        'actions' => ['index', 'ask', 'choice', 'question-detail-view'],
+                        'allow' => true,
+                        'roles' => [
+                            User::ROLE_ADMIN,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         $questions = QuestionForm::find()->all();
