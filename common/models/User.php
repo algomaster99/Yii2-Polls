0737c5109be9fb5f385fc6c202f38cac86d40a3d
Aman Sharma <mannu.poski10@gmail.com>
2018-08-07 01:12:33 +0530

    Added roles for admin and user

--- common/models/User.php
+++ common/models/User.php
@@ -17,6 +17,7 @@ use yii\web\IdentityInterface;
  * @property string $email
  * @property string $auth_key
  * @property integer $status
+ * @property integer $role
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
@@ -25,7 +26,9 @@ class User extends ActiveRecord implements IdentityInterface
 {
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
-
+    const ROLE_MODERATOR = 20;
+    const ROLE_ADMIN = 30;
+    const ROLE_USER = 10;
 
     /**
      * {@inheritdoc}
