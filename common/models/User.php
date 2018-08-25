7420690efbcbfae00a8f4d02f83d015c7ae84f1f
Aman Sharma <mannu.poski10@gmail.com>
2018-08-25 09:12:01 +0530

    Explain the $role variable

--- common/models/User.php
+++ common/models/User.php
@@ -17,7 +17,7 @@ use yii\web\IdentityInterface;
  * @property string $email
  * @property string $auth_key
  * @property integer $status
- * @property integer $role
+ * @property integer $role // added manually to define role of users
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
